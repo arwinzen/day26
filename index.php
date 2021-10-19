a5d4a05080ccc72fd9d3680c7f8587ce2ffd6b92
rw1n <arwingoo90@gmail.com>
2021-10-19 19:16:46 +0800

    SET Bootcamp: day 7 php assignment

---
+++ index.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ <meta charset="UTF-8">
+ <meta name="viewport" content="width=device-width,initial-scale=1.0">
+ <title>Day 7 Assignment</title>
+ <link rel="stylesheet" href="">
+ <style>
+        *{ 
+            padding: 0; 
+            margin: 0; 
+        }
+        html {
+            box-sizing: border-box;
+            font-family: BlinkMacSystemFont, -apple-system, "Segoe UI", "Roboto", "Helvetica Neue", "Arial", sans-serif; 
+            font-size: calc(1.5vh + 1vw + 1%); 
+            line-height: 1.5;
+        }
+        body{ 
+            overflow: auto; 
+            min-height: 100vh; 
+            width: 100%; 
+            padding: 0.5rem 1rem;   
+        }
+        main,
+        header{
+            padding: 0.5rem 2rem;
+        }
+    </style>
+</head>
+<body>
+    <h1>PHP Day 7 Assignment (Arwin Goo Zen)</h1>
+    <?php
+        // Question 1
+        // a. List out stars in a 5 * 5 triangular pattern
+        echo "<h2>Question 1</h2>\n";
+        $height = 5;
+        for ($i = "*"; strlen($i) <= $height; $i .= "*"){
+            echo "${i}"."<br>";
+        }
+
+        // b. list out a reverse pattern of question 1a
+        for ($i = $height; $i >= 0; $i--){
+            echo str_repeat("*", $i)."<br>";
+        }
+
+        // Question 2
+        // Create a script using a for loop to add all the integers between 0 and 30 and display the total.
+        echo "<h2>Question 2</h2>\n";
+        $sum = 0;
+        $start_val = 30;
+        for ($i = 1; $i <= $start_val; $i++){
+            $sum += $i;
+        }
+        echo "The sum of all numbers between 0 and 30 is $sum".".<br>";
+        echo "<br>";
+
+        //Question 3
+        // Create a loop that reads numbers 30 to 50, sum up all numbers which are of odd numbers.
+        echo "<h2>Question 3</h2>\n";
+        $sum_odd = 0;
+        $end_val = 50;
+        echo "Numbers between 30 and 50 : ";
+        for ($i = $start_val; $i <= $end_val; $i++){
+            if ($i === $end_val){
+                echo "$i"."."."<br>";
+            } else {
+                echo "$i".", ";
+            }
+            if ($i % 2 !== 0){
+                $sum_odd += $i;
+            }
+        }
+        echo "<br>";
+        echo "The sum of odd numbers between 30 and 50 is ". "$sum_odd".".<br>";
+        echo "<br>";
+        // Question 4 
+        // Assign the string "Hello there my name is Kevin" to a variable. Read each string and replace spaces with %. Do a count to get the number of % replaced in this statement.
+        echo "<h2>Question 4</h2>\n";
+        $greeting = "Hello there, my name is Kevin.";
+        $mod_greeting = "";
+        $replacements = 0;
+        for ($i = 0; $i < strlen($greeting); $i++){
+            $char = $greeting[$i];
+            if ($char === " "){
+                $replacements++;
+                $mod_greeting .= str_replace(" ", "%", $char);
+            } else {
+                $mod_greeting .= $char;
+            }
+        }
+        echo $mod_greeting."<br>";
+        echo "The number of replacements is $replacements".".<br>";
+
+    ?>
+</body>
+</html>
